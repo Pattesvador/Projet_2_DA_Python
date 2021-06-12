2c1f97660f467208341ae6e6874e3d3e06f054bf
Pattesvador <Pattesvador@hotmail.fr>
2021-06-12 14:53:54 +0200

    Récupération d'une liste de dictionnaires au lieu d'une liste d'url

--- categories_list.py
+++ categories_list.py
@@ -8,13 +8,19 @@ def gather_categories(url):
     else:
         soup = BeautifulSoup(response.text, "html.parser")
         # categories = soup.find("div", class_="side_categories").find_all("a")
-        categories_url = []
+        categories_infos = []
+        part_url = "https://books.toscrape.com/"
         for x in soup.find("div", class_="side_categories").find_all("a"):
         # for x in categories:
-            categories_url.append("https://books.toscrape.com/" + x.get("href"))
-        categories_url.pop(0)
+            category_infos = {
+                "category_url": part_url + x.get("href"),
+                "category_title": soup.find("li", class_="active").text,
+                "category_csv": category_infos["category_title"] + ".csv"
+            }
+            categories_infos.append("https://books.toscrape.com/" + x.get("href"))
+        categories_infos.pop(0)
 
-    return categories_url
+    return categories_infos
 
 
 var = "\n".join(gather_categories("https://books.toscrape.com/index.html"))
