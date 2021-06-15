f0a1dc4defaa727f6fe8706596ed6d6f426d74d7
Pattesvador <Pattesvador@hotmail.fr>
2021-06-15 19:39:23 +0200

    la fonction gather_books retourne deux valeurs, la liste des lelivres de la catégorie et le nom du fichier .csv

--- category_books_list.py
+++ category_books_list.py
@@ -2,9 +2,10 @@ import requests
 from bs4 import BeautifulSoup
 import csv
 
-def gather_books(url):
+
+def gather_books(url, books_list):
     response = requests.get(url)
-    books_list = []
+    # books_list = []
     if not response:
         print("Nope!")
     else:
@@ -18,13 +19,13 @@ def gather_books(url):
         if soup.find("li", class_="next"):
             # print(soup.find("li", class_="next").find("a").get('href'))
             next_page_part_url = soup.find("li", class_="next").find("a").get("href")
-            gather_books(construct_url_2(response.url, next_page_part_url))
-
+            gather_books(construct_url_2(response.url, next_page_part_url), books_list)
 
     with open(file_title, "w") as csvfile:
         writer = csv.writer(csvfile)
         writer.writerow([header_title])
-    return books_list
+
+    return [books_list, file_title]
 
 
 def construct_url(url, part_url):
@@ -35,7 +36,6 @@ def construct_url(url, part_url):
     url = url + part_url
 
     return url
-
 
 
 def construct_url_2(url, part_url):
@@ -50,8 +50,7 @@ def construct_url_2(url, part_url):
 # var = "\n".join(gather_books("https://books.toscrape.com/catalogue/category/books/travel_2/index.html"))
 # print(var)
 # print(len(gather_books("https://books.toscrape.com/catalogue/category/books/travel_2/index.html")))
-var = "\n".join(gather_books("https://books.toscrape.com/catalogue/category/books/mystery_3/page-1.html"))
-print(var)
-print(len(gather_books("https://books.toscrape.com/catalogue/category/books/mystery_3/page-1.html")))
+var = "\n".join(gather_books("https://books.toscrape.com/catalogue/category/books/mystery_3/index.html", books_list=[])[0])
+print(var, len(gather_books("https://books.toscrape.com/catalogue/category/books/mystery_3/index.html", books_list=[])[0]))
 # var = "\n".join(gather_books("https://books.toscrape.com/catalogue/category/books/sequential-art_5/index.html", books_list = []))
 # print(var)
