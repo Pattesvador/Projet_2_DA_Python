00914c74438cfd4e9a13632ccbcf58b56f24a73e
Pattesvador <Pattesvador@hotmail.fr>
2021-06-17 16:47:13 +0200

    Tests d'écriture sur fichier .csv en récupérant les valeurs d'un dictionnaire

--- csv_tests.py
+++ csv_tests.py
@@ -4,8 +4,9 @@ import csv
 # from bs4 import BeautifulSoup
 # from page import scrap_page
 
+dico = {"valeur 1": 1, "valeur 2": 2, "valeur 3": 3}
 with open("tests.csv", "w", newline='') as file:
     writer = csv.writer(file)
-    writer.writerow((1, 2, 3, 4, 5))
-    writer.writerow(["12345"])
+    writer.writerow(dico.values())
+    # writer.writerow(["12345"])
 
