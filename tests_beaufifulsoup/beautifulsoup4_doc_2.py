20f5808f9458ab89c74a78f4cc2bc93fcf722855
Pattesvador <Pattesvador@hotmail.fr>
2022-01-18 15:57:49 +0100

    suppression de fichiers inutiles

--- tests_beaufifulsoup/beautifulsoup4_doc_2.py
+++
@@ -1,6 +0,0 @@
-from bs4 import BeautifulSoup
-
-markup = "<b><!--Hey, buddy. Want to buy a used parser?--></b>"
-soup = BeautifulSoup(markup, 'html.parser')
-comment = soup.b.string
-print("\n", comment, "\n")
